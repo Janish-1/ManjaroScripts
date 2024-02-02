c7554a9cd23b4a39a8be8e9f50a32133e2dabb83
Janish-1 <janish.pancholi11@gmail.com>
2024-02-02 11:08:34 +0530

    more installers

---
+++ java_install.sh
@@ -0,0 +1,2 @@
+sudo pacman -Ss jdk
+sudo pacman -S jdk-openjdk
---
+++ node_install.sh
@@ -0,0 +1,4 @@
+sudo pacman -S nvm
+echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.bashrc
+echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.zshrc
+nvm install --lts
