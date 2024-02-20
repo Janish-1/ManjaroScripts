b0b1e5a3f37f9d667792e9bd146a743a60916259
Janish-1 <janish.pancholi11@gmail.com>
2024-02-21 01:06:57 +0530

    added mount

---
+++ mount.sh
@@ -0,0 +1,12 @@
+sudo mkdir /mnt/Coding
+sudo mkdir /mnt/Data-Janish
+sudo mkdir /mnt/ManjaroScripts
+sudo mkdir /mnt/Pankaj-Pendrive
+sudo mkdir /mnt/PC-Data
+sudo mkdir /mnt/shreya-album
+sudo mount -t cifs //192.168.1.2/Coding /mnt/Coding -o username=JANISH
+sudo mount -t cifs //192.168.1.2/Data-Janish /mnt/Data-Janish -o username=JANISH
+sudo mount -t cifs //192.168.1.2/ManjaroScripts /mnt/ManjaroScripts -o username=JANISH
+sudo mount -t cifs //192.168.1.2/Pankaj-Pendrive /mnt/Pankaj-Pendrive -o username=JANISH
+sudo mount -t cifs //192.168.1.2/PC-Data /mnt/PC-Data -o username=JANISH
+sudo mount -t cifs //192.168.1.2/shreya-album /mnt/shreya-album -o username=JANISH
