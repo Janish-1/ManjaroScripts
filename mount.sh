47536da4cbf12ae7a66316049398b2b049d89f80
Janish-1 <janish.pancholi11@gmail.com>
2024-02-24 11:14:25 +0530

    daily commit

--- mount.sh
+++ mount.sh
@@ -4,9 +4,9 @@ sudo mkdir /mnt/ManjaroScripts
 sudo mkdir /mnt/Pankaj-Pendrive
 sudo mkdir /mnt/PC-Data
 sudo mkdir /mnt/shreya-album
-sudo mount -t cifs //192.168.1.2/Coding /mnt/Coding -o username=JANISH
-sudo mount -t cifs //192.168.1.2/Data-Janish /mnt/Data-Janish -o username=JANISH
-sudo mount -t cifs //192.168.1.2/ManjaroScripts /mnt/ManjaroScripts -o username=JANISH
-sudo mount -t cifs //192.168.1.2/Pankaj-Pendrive /mnt/Pankaj-Pendrive -o username=JANISH
-sudo mount -t cifs //192.168.1.2/PC-Data /mnt/PC-Data -o username=JANISH
-sudo mount -t cifs //192.168.1.2/shreya-album /mnt/shreya-album -o username=JANISH
+sudo mount -t cifs //192.168.1.4/Coding /mnt/Coding -o username=JANISH
+sudo mount -t cifs //192.168.1.4/Data-Janish /mnt/Data-Janish -o username=JANISH
+sudo mount -t cifs //192.168.1.4/ManjaroScripts /mnt/ManjaroScripts -o username=JANISH
+sudo mount -t cifs //192.168.1.4/Pankaj-Pendrive /mnt/Pankaj-Pendrive -o username=JANISH
+sudo mount -t cifs //192.168.1.4/PC-Data /mnt/PC-Data -o username=JANISH
+sudo mount -t cifs //192.168.1.4/shreya-album /mnt/shreya-album -o username=JANISH
