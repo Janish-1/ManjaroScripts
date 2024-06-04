258109b6525692b10ec70945341ff73ff468c0aa
Janish-1 <janish.pancholi11@gmail.com>
2024-06-05 00:50:45 +0530

    Fixed

--- ServiceInstallers/mariadb_setup.sh
+++ ServiceInstallers/mariadb_setup.sh
@@ -2,4 +2,5 @@ sudo pacman -Syu mariadb
 sudo mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 sudo systemctl enable --now mariadb
 sudo systemctl status mariadb
+sudo su
 mariadb-secure-installation
