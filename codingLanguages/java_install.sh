e659b363a7d3f8354955e9cf778bb43860d65d35
Janish-1 <janish.pancholi11@gmail.com>
2024-10-27 17:26:23 +0530

    revert

--- codingLanguages/java_install.sh
+++ codingLanguages/java_install.sh
@@ -1,2 +1,2 @@
-sudo pacman -Syu jdk11-openjdk
-echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk" >> ~/.bashrc
+sudo pacman -Syu jdk17-openjdk
+echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk" >> ~/.bashrc
