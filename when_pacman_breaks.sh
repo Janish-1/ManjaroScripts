938a367d1e7ff695b4ce50e793a96315905ee1e3
Janish-1 <janish.pancholi11@gmail.com>
2024-11-30 20:03:01 +0530

    more pacman breaks

--- when_pacman_breaks.sh
+++ when_pacman_breaks.sh
@@ -1,2 +1,8 @@
 sudo pacman-mirrors -f5 && sudo pacman -Syyu
 sudo pacman-mirrors --geoip
+sudo pacman-mirrors --fasttrack 5 && sudo pacman -Syu
+sudo pacman-mirrors --api --set-branch testing
+sudo pacman-mirrors --api --set-branch stable
+sudo pacman-mirrors --api --set-branch unstable
+sudo pacman-mirrors -id
+sudo pacman-mirrors -c all
